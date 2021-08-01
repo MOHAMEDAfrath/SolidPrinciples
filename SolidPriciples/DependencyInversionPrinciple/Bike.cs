aad63e7ad2457a213b270fedf69dfae8448055a5
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-01 18:10:14 +0530

    [Afrath]Updated the file with Dependency Inversion Priciple

---
+++ SolidPriciples/DependencyInversionPrinciple/Bike.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPriciples.DependencyInversionPrinciple
+{
+    //High level module
+    //Not dependent upon low level module
+    public class AutomobileManager
+    {
+        private IAutoMobile autoMobile;
+        public AutomobileManager(IAutoMobile autoMobile)
+        {
+            this.autoMobile = autoMobile;
+        }
+        public void Display()
+        {
+            autoMobile.Wheels();
+        }
+    }
+    public interface IAutoMobile
+    {
+        void Wheels();
+    }
+    //Low Level module
+    public class Car : IAutoMobile
+    {
+        public void Wheels()
+        {
+            Console.WriteLine("Four Wheels");
+        }
+    }
+    public class Bike : IAutoMobile
+    {
+        public void Wheels()
+        {
+            Console.WriteLine("Two Wheels");
+        }
+    }
+}
--- SolidPriciples/Program.cs
+++ SolidPriciples/Program.cs
@@ -1,4 +1,5 @@
-﻿using SolidPriciples.InterfaceSegregationPrinciple;
+﻿using SolidPriciples.DependencyInversionPrinciple;
+using SolidPriciples.InterfaceSegregationPrinciple;
 using SolidPriciples.Liskov_Principle;
 using SolidPriciples.OpenClosedPrinciple;
 using SolidPriciples.SingleResponsibilityPrinciple;
@@ -11,25 +12,29 @@ namespace SolidPriciples
         static void Main(string[] args)
         {
             //SRP
-            //BankAccount bank = new BankAccount();
-            //bank.AccountNumber = 55555555555;
-            //bank.AccountHolderName = "Ram";
-            //bank.Balance = 5555.28;
-            //ATM getMoney = new ATM();
-            //getMoney.ATMFunction(bank);
+            Console.WriteLine("*******Single responsibility Principle*******");
+            BankAccount bank = new BankAccount();
+            bank.AccountNumber = 55555555555;
+            bank.AccountHolderName = "Ram";
+            bank.Balance = 5555.28;
+            ATM getMoney = new ATM();
+            getMoney.ATMFunction(bank);
 
             //OCP
-            //FindArea findArea = new Square(5.55);
-            //FindArea find = new Circle(5.98);
-            //Console.WriteLine(findArea.Area());
-            //Console.WriteLine(find.Area());
+            Console.WriteLine("*******Open closed Principle*******");
+            FindArea findArea = new Square(5.55);
+            FindArea find = new Circle(5.98);
+            Console.WriteLine(findArea.Area());
+            Console.WriteLine(find.Area());
 
             //LSP
-            //Colour apple = new Apple();
-            //Colour orange = new Orange();
-            //Console.WriteLine("The colour of orange is " +orange.GetColour());
-            //Console.WriteLine("The colour of apple is " + apple.GetColour());
+            Console.WriteLine("*******Liskov Principle*******");
+            Colour apple = new Apple();
+            Colour orange = new Orange();
+            Console.WriteLine("The colour of orange is " + orange.GetColour());
+            Console.WriteLine("The colour of apple is " + apple.GetColour());
             //ISP
+            Console.WriteLine("*******Interface segregation Principle*******");
             Console.WriteLine("Online Order");
             IOnlineOrder onlineOrder = new OnlineOrder();
             onlineOrder.Order();
@@ -37,7 +42,17 @@ namespace SolidPriciples
             Console.WriteLine("Offline order");
             IOfflineOrder offlineOrder = new PayOnDelivery();
             offlineOrder.Order();
-            
+            //DIP where high level module depends on abstraction not on low level modules
+            Console.WriteLine("*******Dependency Inversion Principle*******");
+            IAutoMobile car = new Car();
+            AutomobileManager automobilemanager = new AutomobileManager(car);
+            automobilemanager.Display();
+            IAutoMobile bike = new Bike();
+            AutomobileManager automobilemanager1 = new AutomobileManager(bike);
+            automobilemanager1.Display();
+
+
+
         }
     }
 }
