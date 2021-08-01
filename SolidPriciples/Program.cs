bd30dd72645bb91342f281326a6e08b647488968
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-01 15:41:14 +0530

    [Afrath]Welcome to Solid principles

---
+++ SolidPriciples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidPriciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
